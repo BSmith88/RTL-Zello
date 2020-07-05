dcc9830c0fa0bf3932d6a2a11f4badc514cb2966
blackwellj <blackwellj@hotmail.co.uk>
2020-07-05 10:11:08 +0100

    Update start_0.sh

--- start_0.sh
+++ start_0.sh
@@ -1,14 +1,16 @@
 #! /bin/sh
 
-FREQUENCY="145.0"
+FREQUENCY="156.0"
 GAIN="50"
 SQUELCH="280"
 USER="VHF Channel 0"
-PASSWORD="RNLIhmcg999"
+PASSWORD="password"
 CHANNEL="VHF CH0"
 
-TOKEN="eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJXa002WlcxdE16ZzJMbVY0WlRveS5HY01oKzJNQ1dMamU5TzQwbmk5bGMrNXJzQ3llXC9pRXRpdVV1dDRSNU1yST0iLCJleHAiOjE1OTMyNzM3MTgsImF6cCI6ImRldiJ9.Fu66yGaqdGQ0K+L0DGaNbyyi3xfBimd+paHHl9C36Mzr+FgypJWQ4icwSj0kYN9ocW6qeluhuK4xBzTIXSg/ue0dtGqkxAJxxUWoN4i0ASgFeyBCD2ZfSoI5hSge0fPLCc9KACFIacyO2P2PzII2kWaErMS7+cvbu/YC+COINb8pDF8mcViQ3NKo1/Zf3QwpPl1ksBzjLr/A6gPd6vrlWP992mgpKUI3DTxtm8tYw3LOKqd+b28mqApu9WP5SwFF7I3eUSN3V/+/7665+Wxvid+1wv7dlXQEDqKMJhpw9ioeoeM4KYbbkV8dQFVfp+/1iISe2ImN9OsWfm0w7S7bAg=="
+TOKEN="eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJXa002VFRaT1RWTTZNUT09LmYxUkZ0MUk2NzlVZG9zYmQxb0tyOW92MXcwTVZ4Y2lzTzJpVTNSMFpjbUU9IiwiZXhwIjoxNTkzODc4NzgxfQ==.wOQE7lNqSrZm5IuTD8TPxiGKrW+d0Jpd/G9/SfExnVsxK0wOBYeHMc3VFZUIVD9vOGkTEqctQ4y46T+us+C58WCMyqnqZ5F7BYwC7a3SlY0XvFlddmZuEoU0o+h5Lknpp0+uBIvIcYo5q53H3vz12nIjZLJd9UwqgP3RPO8xaz3AkbcMGTnmIDT5DgsKB6ZdZKZt5BLIko9ZbERgwgU+b/tN64jWbyGmlw+cqGMQIMplBuYFQHQYmRpeXrTqfPtVjxvnzXOJi3jpJ8PW24kxvX0Mzx2P5P1/GpSTBgbsiJkv97aZLDBqfWs0qzinfMp6wkM3yb6x3uCXSfOgEPy5CQ=="
 
-rtl_fm -M fm -f "$FREQUENCY"M -s 12k -g "$GAIN" -l "$SQUELCH" | pacat &
+pulseaudio -D
 
-./nrad --token "$TOKEN" --user $USER --password $PASSWORD --channel $CHANNEL --vox-threshold 1
+rtl_fm -M fm -f "$FREQUENCY"M -s 12k -g "$GAIN" -l "$SQUELCH" | play -r 32k -t raw -e s -b 16 -c 1 -V1 - &
+
+./nrad --token "$TOKEN" --user "$USER" --password "$PASSWORD" --channel "$CHANNEL" --vox-threshold 1

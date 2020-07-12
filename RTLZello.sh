bcc59194276450658e1f4bf431409b3ed8a079e9
blackwellj <blackwellj@hotmail.co.uk>
2020-07-12 18:31:02 +0100

    Update RTLZello.sh

--- RTLZello.sh
+++ RTLZello.sh
@@ -10,6 +10,6 @@ PASSWORD="password"
 CHANNEL="channel"
 VOX="1"
 
-TOKEN=`./tokenmanager key.txt <issuer string>`
+TOKEN=`./RTL-Zello/tokenmanager RTL-Zello/key.txt <issuer string>`
 
-rtl_fm -M "$MODE" -f "$FREQUENCY"M -s 16k -g "$GAIN" -l "$SQUELCH" | ./nrad --token "$TOKEN" --user "$USER" --password "$PASSWORD" --channel "$CHANNEL" --audio-source - --vox-threshold "$VOX"
+rtl_fm -M "$MODE" -f "$FREQUENCY"M -s 16k -g "$GAIN" -l "$SQUELCH" | ./RTL-Zello/nrad --token "$TOKEN" --user "$USER" --password "$PASSWORD" --channel "$CHANNEL" --audio-source - --vox-threshold "$VOX"
